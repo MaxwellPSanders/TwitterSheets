f226cb2f3c4c9ec99064dcc35b37b1e500956f8f
Gabe Comer <gabriel.comer@mavs.uta.edu>
2019-06-09 18:53:08 -0500

    Added check for tweet length

--- post_tweet.py
+++ post_tweet.py
@@ -5,12 +5,12 @@ CONSUMER_SECRET = 'XXXX'
 ACCESS_KEY = 'XXXX-XXXX'
 ACCESS_SECRET = 'XXXX'
 
-def post_tweet( tweet_content ) :
+def post_tweet( tweet_you, tweet_me ) :
 
     auth = tweepy.OAuthHandler( CONSUMER_KEY, CONSUMER_SECRET )
     auth.set_access_token( ACCESS_KEY, ACCESS_SECRET )
 
-    tweet = "You:\nMe, woke: " + tweet_content
-
-    api = tweepy.API( auth )
-    api.update_status( tweet )
+    tweet = "You: " + tweet_you + "\nMe, woke: " + tweet_me
+    if len( tweet ) < 280 :
+        api = tweepy.API( auth )
+        api.update_status( tweet )
